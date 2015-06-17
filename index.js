679177ffe04300ffbee32001ed007b81fee5bf3a
juji <jujiyangasli@gmail.com>
2015-06-18 01:11:17 +0700

    auto-commit

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var Q = require('q');
 module.exports = exports = function(q){
 	q.forEach = function(arr,func){
-		var p = Q(0);
-		var ret = [];
-		for(var i in arr) 
-			p = func(arr[i],i)
-			.then(function(r){ret.push({err:null,result:r});})
-			.fail(function(e){ret.push({err:e,result:null});});
+		var p = Q(0),
+			ret = [],
+			idx = [];
+		for(var i in arr) idx.push(i);
+		for(var i in idx)
+			p = p.then(function(ii){ return func(arr[idx[ii]],idx[ii]) })
+				.then(function(r){ret.push({err:null,result:r}); return ++ii; })
+				.fail(function(e){ret.push({err:e,result:null}); return ++ii; });
 		return p.then(function(){ return ret; });
 	};
 	
