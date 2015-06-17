27eae69bae28f9d44a4018997fd374b4e6c1579e
juji <jujiyangasli@gmail.com>
2015-06-18 01:24:04 +0700

    auto-commit

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ module.exports = exports = function(q){
 			idx = [];
 		for(var i in arr) idx.push(i);
 		for(var i in idx)
-			p = p.then(function(ii){ return func(arr[idx[ii]],idx[ii]); })
-				.then(function(r){ret.push({err:null,result:r}); return ++ii; })
-				.fail(function(e){ret.push({err:e,result:null}); return ++ii; });
+			p = p.then(function(ii){ 
+					return func(arr[idx[ii]],idx[ii])
+					.then(function(r){ ret.push({err:null,result:r}); return ++ii; })
+					.fail(function(e){ ret.push({err:e,result:null}); return ++ii; });
+				});
 		return p.then(function(){ return ret; });
 	};
 	
